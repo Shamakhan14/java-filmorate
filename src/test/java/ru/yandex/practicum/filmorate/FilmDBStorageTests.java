6601c562e0725e18fa971c1a93a683123d035654
Shamakhan <valentin-u-a@yandex.ru>
2022-12-15 17:52:03 +0300

    feat: снова исправлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/FilmDBStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmDBStorageTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.dao.FilmDbStorage;
 import ru.yandex.practicum.filmorate.dao.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -16,19 +17,18 @@ import ru.yandex.practicum.filmorate.model.User;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class FilmDBStorageTests {
 
     private final FilmDbStorage filmDbStorage;
     private final UserDbStorage userStorage;
 
     @Test
-    public void test() {
-        //creating
+    public void shouldCreateFilm() {
         Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
                 new Rating(1, "G"));
         Film film2 = filmDbStorage.addFilm(film1);
@@ -37,16 +37,27 @@ public class FilmDBStorageTests {
         assertEquals(film1.getReleaseDate(), film2.getReleaseDate());
         assertEquals(film1.getDuration(), film2.getDuration());
         assertEquals(film1.getMpa(), film2.getMpa());
+    }
 
-        //finding
+    @Test
+    public void shouldFindFilm() {
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
         Film film3 = filmDbStorage.findFilm(film2.getId());
         assertEquals(film3.getName(), film2.getName());
         assertEquals(film3.getDescription(), film2.getDescription());
         assertEquals(film3.getReleaseDate(), film2.getReleaseDate());
         assertEquals(film3.getDuration(), film2.getDuration());
         assertEquals(film3.getMpa(), film2.getMpa());
+    }
 
-        //updating
+    @Test
+    public void shouldUpdateFilm() {
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
+        Film film3 = filmDbStorage.findFilm(film2.getId());
         film3.setName("3name");
         filmDbStorage.updateFilm(film3);
         assertEquals(film2.getId(), film3.getId());
@@ -55,21 +66,49 @@ public class FilmDBStorageTests {
         assertEquals(film2.getReleaseDate(), film3.getReleaseDate());
         assertEquals(film2.getDuration(), film3.getDuration());
         assertEquals(film2.getMpa(), film3.getMpa());
+    }
 
-        //getting all
+    @Test
+    public void shouldGetAllFilms() {
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
         assertEquals(1, filmDbStorage.getFilms().size());
+    }
 
-        //adding like
+    @Test
+    public void shouldAddLike() {
         User user1 = new User("1email@email.com", "login1", "name1", LocalDate.now());
         User user2 = userStorage.addUser(user1);
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
         filmDbStorage.addLike(film2.getId(), user2.getId());
         assertEquals(1, filmDbStorage.getLikedIDs(film2.getId()).size());
+    }
 
-        //removing like
+    @Test
+    public void shouldRemoveLike() {
+        User user1 = new User("1email@email.com", "login1", "name1", LocalDate.now());
+        User user2 = userStorage.addUser(user1);
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
+        filmDbStorage.addLike(film2.getId(), user2.getId());
+        assertEquals(1, filmDbStorage.getLikedIDs(film2.getId()).size());
         filmDbStorage.removeLike(film2.getId(), user2.getId());
         assertEquals(0, filmDbStorage.getLikedIDs(film2.getId()).size());
+    }
 
-        //getting top films
+    @Test
+    public void shouldGetTopFilms() {
+        User user1 = new User("1email@email.com", "login1", "name1", LocalDate.now());
+        User user2 = userStorage.addUser(user1);
+        Film film1 = new Film("1name", "1description", LocalDate.now(), 100,
+                new Rating(1, "G"));
+        Film film2 = filmDbStorage.addFilm(film1);
+        filmDbStorage.addLike(film2.getId(), user2.getId());
+        assertEquals(1, filmDbStorage.getLikedIDs(film2.getId()).size());
         assertEquals(1, filmDbStorage.getTopFilms(1).size());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserDBStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserDBStorageTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.dao.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -15,27 +16,35 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class UserDBStorageTests {
 
     private final UserDbStorage userStorage;
 
     @Test
-    public void test() {
-        //creating
+    public void shouldCreateUser() {
         User user = new User("email@email.com", "login", "name", LocalDate.now());
         User user2 = userStorage.addUser(user);
         assertEquals(user.getName(), user2.getName());
         assertEquals(user.getEmail(), user2.getEmail());
         assertEquals(user.getLogin(), user2.getLogin());
+    }
 
-        //finding
+    @Test
+    public void shouldFindUser() {
+        User user = new User("email@email.com", "login", "name", LocalDate.now());
+        User user2 = userStorage.addUser(user);
         User user3 = userStorage.findUser(user2.getId());
         assertEquals(user2.getId(), user3.getId());
         assertEquals(user2.getName(), user3.getName());
         assertEquals(user2.getEmail(), user3.getEmail());
         assertEquals(user2.getLogin(), user3.getLogin());
+    }
 
-        //updating
+    @Test
+    public void shouldUpdateUser() {
+        User user = new User("email@email.com", "login", "name", LocalDate.now());
+        User user2 = userStorage.addUser(user);
         user2.setName("name2");
         userStorage.updateUser(user2);
         User user4 = userStorage.findUser(user2.getId());
@@ -43,17 +52,31 @@ public class UserDBStorageTests {
         assertEquals("name2", user2.getName());
         assertEquals(user4.getEmail(), user2.getEmail());
         assertEquals(user4.getLogin(), user2.getLogin());
+    }
 
-        //getting all
+    @Test
+    public void shouldGetAllUsers() {
+        User user = new User("email@email.com", "login", "name", LocalDate.now());
+        User user2 = userStorage.addUser(user);
         assertEquals(1, userStorage.getUsers().size());
+    }
 
-        //adding friend
+    @Test
+    public void shouldAddFriend() {
+        User user = new User("email@email.com", "login", "name", LocalDate.now());
+        User user2 = userStorage.addUser(user);
         User user5 = new User("3email@email.com", "login3", "name3", LocalDate.now());
-        User user6 = userStorage.addUser(user3);
+        User user6 = userStorage.addUser(user5);
         userStorage.addFriend(user2.getId(), user6.getId());
         assertEquals(1, userStorage.getFriends(user2.getId()).size());
+    }
 
-        //removing friend
+    @Test
+    public void shouldRemoveFriend() {
+        User user = new User("email@email.com", "login", "name", LocalDate.now());
+        User user2 = userStorage.addUser(user);
+        User user5 = new User("3email@email.com", "login3", "name3", LocalDate.now());
+        User user6 = userStorage.addUser(user5);
         userStorage.removeFriend(user2.getId(), user6.getId());
         assertEquals(0, userStorage.getFriends(user2.getId()).size());
     }
